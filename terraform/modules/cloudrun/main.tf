16af2be1158573a526d1bba372dd27e10bed2c67
agonzalezmtz <agonzalezmtz@google.com>
2025-10-22 15:28:53 -0600

    Update main.tf
    Test Clourun feature

---
+++ terraform/modules/cloudrun/main.tf
@@ -0,0 +1,39 @@
+# Meli-TF/terraform/modules/cloudrun/variables.tf
+
+variable "project_id" {
+  description = "The GCP project ID where the service will be deployed."
+  type        = string
+}
+
+variable "location" {
+  description = "The GCP region for the service (e.g., 'us-central1')."
+  type        = string
+}
+
+variable "service_name" {
+  description = "The name of the Cloud Run service."
+  type        = string
+}
+
+variable "image_name" {
+  description = "The full path to the container image (e.g., 'gcr.io/my-project/my-image:latest')."
+  type        = string
+}
+
+variable "allow_unauthenticated" {
+  description = "If true, allows public, unauthenticated access to the service."
+  type        = bool
+  default     = false
+}
+
+variable "container_port" {
+  description = "The port your container listens on."
+  type        = number
+  default     = 8080
+}
+
+variable "environment_variables" {
+  description = "A map of environment variables to set in the container."
+  type        = map(string)
+  default     = {}
+}
