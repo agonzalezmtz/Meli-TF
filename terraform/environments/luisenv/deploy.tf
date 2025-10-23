7fef05a5228c9e45cd3efe767c64623b5bc73643
Alonso Gonzalez <agonzalezmtz@google.com>
2025-10-22 22:06:08 -0600

    Delete

--- terraform/environments/luisenv/deploy.tf
+++ terraform/environments/luisenv/deploy.tf
@@ -1,37 +1,37 @@
-# Meli-TF/terraform/environments/luisenv/main.tf
+# # Meli-TF/terraform/environments/luisenv/main.tf
 
-# Define the required Google Cloud provider.
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 7.0"
-    }
-  }
-}
+# # Define the required Google Cloud provider.
+# terraform {
+#   required_providers {
+#     google = {
+#       source  = "hashicorp/google"
+#       version = "~> 7.0"
+#     }
+#   }
+# }
 
-# Call our custom "cloudrun" module.
-module "test_meli_service" {
-  # Relative path to the module directory from this file.
-  source = "../../modules/cloudrun"
+# # Call our custom "cloudrun" module.
+# module "test_meli_service" {
+#   # Relative path to the module directory from this file.
+#   source = "../../modules/cloudrun"
 
-  # --- Pass variables to the module ---
+#   # --- Pass variables to the module ---
 
-  # The GCP Project ID to deploy into.
-  # CHANGE THIS 
-  project_id = "1034146075509"
+#   # The GCP Project ID to deploy into.
+#   # CHANGE THIS 
+#   project_id = "1034146075509"
 
-  location              = "us-central1"
-  service_name          = "test-meli"
-  image_name            = "docker.io/nginx:latest" # Standard Google test image
-  allow_unauthenticated = false                     # Make the service public
-  container_port = 80
-  environment_variables = {}
-}
+#   location              = "us-central1"
+#   service_name          = "test-meli"
+#   image_name            = "docker.io/nginx:latest" # Standard Google test image
+#   allow_unauthenticated = false                     # Make the service public
+#   container_port = 80
+#   environment_variables = {}
+# }
 
 
-# Output the URL of the service after it's deployed.
-output "service_url" {
-  description = "The URL of the deployed test service."
-  value       = module.test_meli_service.service_url
-}
+# # Output the URL of the service after it's deployed.
+# output "service_url" {
+#   description = "The URL of the deployed test service."
+#   value       = module.test_meli_service.service_url
+# }
